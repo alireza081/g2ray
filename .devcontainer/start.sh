ff2709e20ecb4593fbdc2f10c430512ae860859d
alireza081 <saghafialireza081@gmail.com>
2026-05-10 12:21:11 +0330

    Create start.sh

---
+++ .devcontainer/start.sh
@@ -0,0 +1,61 @@
+188.137.241.174
+98M/nJxcR2k.X9\
+
+ky4n43zyl0k@erzi.me
+Alireza@1646
+
+our4xvs4jwk@erzi.me
+
+https://github.com/Musixal/Rathole-Tunnel
+(bash <(curl -Ls --ipv4 https://raw.githubusercontent.com/Musixal/rathole-tunnel/main/rathole.sh))
+
+https://www.who.int/publications/i/item/9789240086517
+https://www.iihs.org/research-areas/seat-belts
+
+AKfycbyMBdNAMavJe_nzUU6xlmNED9pR6vCPhZWSSYj-BOQ2KitQka_TLu4_3j3ZDSY-knoz
+Ehsan@1646
+AKfycbzGDSDHNsCrqcUvReyM4hVlFCs4zOJ8ooLp_3vQh2l4rJ66F0bFxG82qIT93dgC-mMAgg
+Ehsan@1234
+AKfycbzCZokDIU3j8zQLTyBnvJsCCNdm3q84xi2s7mNBV5oAKiLhGqXu556QxDSmVVZTsyVf
+1f364b12-65ff-450b-82d8-6dfb9ab249cd
+
+ehsan.ershadi1646@gmail.com
+saq,+9qKGMaqo
+AKfycbzAGYxbUgUEhQiDqWqRrMfcFL2EV2wFcMHyGEX-5dapTiKdNP1XcVQZzDOmGUYNUCqT
+
+AKfycby8i_jiExiadegjVSEGR5-SdK88q82O5qtUJqJ7OOB-78w2IlS5SWjRjRc1e8riU4Wu(mhr-cfw)
+
+
+https://www.latinncap.com/en/our-tests/adult-occupant/frontal-impact
+https://www.motortrend.com/features/nhtsa-safety-ratings-explained
+https://patents.google.com/patent/CN112606793A/en
+
+
+
+-VPS- سرور -const EXIT_RELAY_URL = "http://IP ;"relay/
+
+اگه با استفاده از روش گوگل وصل هستید ولی بعضی وب سایت ها به ایپی گیر میدن میتونید با مروگر برید به وب پراکسی ها و از اون جا به وب سایت که میخوایید برید 
+یه سری وب پراکسی ها میزارم پایین .
+
+
+https://proxy.batmite.com/
+https://proxypal.net/
+https://proxyium.com/
+https://www.flyproxy.com/proxysite/
+https://unblockyoutube.video/
+https://proxyium.com/
+https://www.4everproxy.com/
+https://www.sslunblocker.com/
+http://dgsrmada.com:2022/
+https://zilp.com/glype/index.php
+https://power.re/proxy/
+https://power.re/proxy/
+https://glype.azurewebsites.net/index.php
+https://proxy.schel.li/
+
+Lists : 
+https://www.proxydocker.com/en/webproxy/type/Glype
+https://mmoapi.com/web-proxy/phproxy-proxies
+https://mmoapi.com/web-proxy/glype-proxies
+
+@archivetell    05113aabcacc4020f2a54c3e3b493702f86cbcaac30ced80906bcfea27a04883
